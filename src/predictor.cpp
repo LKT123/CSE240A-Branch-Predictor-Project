c9ff3bce1983b239a666f50db545ecda503eea5f
Hengzhou Li <91091681+LKT123@users.noreply.github.com>
2025-02-11 16:16:08 -0500

    Update predictor.cpp

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -543,7 +543,7 @@ void init_predictor()
     init_tournament();
     break;
   case CUSTOM:
-    init_custom();
+    init_tournament();//init_custom();
     break;
   default:
     break;
@@ -567,7 +567,7 @@ uint32_t make_prediction(uint32_t pc, uint32_t target, uint32_t direct)
   case TOURNAMENT:
     return torunament_predict(pc);
   case CUSTOM:
-    return custom_predict(pc);
+    return torunament_predict(pc);//return custom_predict(pc);
   default:
     break;
   }
@@ -594,7 +594,7 @@ void train_predictor(uint32_t pc, uint32_t target, uint32_t outcome, uint32_t co
     case TOURNAMENT:
       return train_tournament(pc, outcome);
     case CUSTOM:
-      return train_custom(pc, outcome);
+      return train_tournament(pc, outcome);//return train_custom(pc, outcome);
     default:
       break;
     }
